242d10dcec95e619fefcbbe6ee9ccb65a85fb029
ICONationDevTeam <iconation.team@gmail.com>
2022-10-08 21:36:29 +0200

    Add CORS

--- api/ticks/public/main.py
+++ api/ticks/public/main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
 from api.ticks.public.routers import tokens, pools, ticks, routing
 from api.ticks.dependencies import create_database
+from fastapi.middleware.cors import CORSMiddleware
 
 # Load DB
 create_database()
 
 # Load API
 app = FastAPI()
+
+origins = [
+  "http://localhost:8080",
+  "https://convexus-sdk-js-webpack-boilerplate.pages.dev"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(tokens.router)
 app.include_router(pools.router)
 app.include_router(ticks.router)
